9859cdfb850554640a02fb86759b65bbc844e34f
Alulelo <hotpotatou@gmail.com>
2021-08-19 21:56:41 +0500

    committttt

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hello World";
+
+           Console.WriteLine( phrase );
+           Console.ReadLine();
+        }
+    }
+}
+
