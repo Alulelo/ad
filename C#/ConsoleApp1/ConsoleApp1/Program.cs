664a4b88139796d9cd10981a6fccbdb7cdf13a55
Alulelo <hotpotatou@gmail.com>
2021-08-21 19:39:14 +0500

    commit

--- C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Your name:");
-            string name = Console.ReadLine();
-            Console.Write("Enter Your age:");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " Your age is  " + age);
+            Console.Write("Enter a number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2 );
             Console.ReadLine();
+
+            string colour, pluralnoun, celebrity;
+
+            Console.Write("Enter a colour:");
+            colour = Console.ReadLine();
+
+            Console.Write("Enter a pluralnoun:");
+            pluralnoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity:");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + colour );
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+            Console.ReadLine();
+
+            int[] num = { 1, 2, 3, 4, 5 };
+            string[] friends = new string [2];
+            friends[0] = "alu";
+            friends[1] = "tamatar";
+
+            Console.WriteLine( friends[0] + " le lo");
+            Console.WriteLine(friends[1] + " le lo");
+
+            Console.ReadLine();
+
+            
         }
     }
 }
