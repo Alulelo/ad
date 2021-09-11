3bf22461618d91832640ffeb11168ff82be13535
Alulelo <hotpotatou@gmail.com>
2021-09-11 22:03:15 +0500

    main

--- C#/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,44 +6,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            bool isboy = true;
+            bool isshort = true;
 
-            Console.Write("Enter another number");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine(num1 + num2 );
-            Console.ReadLine();
-
-            string colour, pluralnoun, celebrity;
-
-            Console.Write("Enter a colour:");
-            colour = Console.ReadLine();
-
-            Console.Write("Enter a pluralnoun:");
-            pluralnoun = Console.ReadLine();
-
-            Console.Write("Enter a celebrity:");
-            celebrity = Console.ReadLine();
-
-            Console.WriteLine("Roses are " + colour );
-            Console.WriteLine(pluralnoun + " are blue");
-            Console.WriteLine("I love " + celebrity);
+            if (isboy&&isshort)
+            {
+                Console.WriteLine("you are a short boi");
+            }
+            else if (isboy && !isshort)
+            {
+                Console.WriteLine("U are a not a short boi");
+            }else if (!isboy&&isshort)
+            {
+                Console.WriteLine("U are short but not a boi");
+            }
+            else
+            {
+                Console.WriteLine("U are not a boi and u are not short");
+            }
 
             Console.ReadLine();
-
-            int[] num = { 1, 2, 3, 4, 5 };
-            string[] friends = new string [2];
-            friends[0] = "alu";
-            friends[1] = "tamatar";
-
-            Console.WriteLine( friends[0] + " le lo");
-            Console.WriteLine(friends[1] + " le lo");
-
-            Console.ReadLine();
-
-            
         }
+  
+
+      
+      
+
+      
     }
 }
 
