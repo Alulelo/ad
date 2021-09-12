3346cce6517820cb8217b9a4de1eff61da93d65a
Alulelo <hotpotatou@gmail.com>
2021-09-12 23:00:59 +0500

    main

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a no1:");
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Operators are as: +(Addition) -(Subtraction) *(Multiplication) /(Divide) ");
+
+            Console.Write("Enter a operator:");
+            string hh = Console.ReadLine();
+
+            Console.Write("Write a no2:");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+            if (hh == "+") 
+            {
+                Console.Write(num1 + num2);
+            }else if (hh == "-") 
+            {
+                Console.Write(num1 - num2);
+            }else if (hh == "*")
+            {
+                Console.Write(num1 * num2 );
+            }
+            else if(hh== "/")
+            {
+                Console.Write(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid operator U dummy");
+            }
+
+            Console.ReadLine();
+
+
+        }
+   
+        
+      
+    }
+}
+
